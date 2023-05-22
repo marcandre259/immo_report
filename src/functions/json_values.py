4b8d47e574ced05f85b88c9ce5da07604efa1d72
marc <marcandrechenier@gmail.com>
2023-05-22 22:38:16 +0200

    Added parallel json value selection -> high overhead lol

--- src/functions/json_values.py
+++ src/functions/json_values.py
@@ -3,6 +3,40 @@
 import json
 from pathlib import Path
 from typing import List
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+
+
+def select_value(filename: str, path: Path, *args):
+    error_flag = False
+    # Probably smart to parralelize that chunk
+    with open(str(path / filename)) as f:
+        data_dict = json.load(f)
+
+    for arg in args:
+        try:
+            data_dict = data_dict[arg]
+        except KeyError:
+            error_flag = True
+            break
+
+    if not error_flag:
+        return data_dict
+
+    else:
+        return None
+
+
+def get_json_values_parallel(file_list: List[str], path: Path, *args, ncores: int = 8):
+    with ProcessPoolExecutor(max_workers=ncores) as executor:
+        futures = [
+            executor.submit(select_value, filename, path, *args)
+            for filename in file_list
+        ]
+
+    results = [future.result() for future in futures]
+
+    return results
 
 
 def get_json_values(file_list: List[str], path: Path, *args):
